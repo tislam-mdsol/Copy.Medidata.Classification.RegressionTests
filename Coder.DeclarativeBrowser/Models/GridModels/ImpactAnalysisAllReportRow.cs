00aee3327ccb75b8ed7e7161f7108922b61386d4
Jo <jtan@mdsol.com>
2016-05-09 15:03:06 -0400

    add page objects

---
+++ Coder.DeclarativeBrowser/Models/GridModels/ImpactAnalysisAllReportRow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coder.DeclarativeBrowser.ExtensionMethods;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace Coder.DeclarativeBrowser.Models.GridModels
+{
+    public class ImpactAnalysisAllReportRow
+    {
+        public string Study { get; set; }
+        public string DictionaryType { get; set; }
+        public string ToVersion { get; set; }
+        public string ToSynonymList { get; set; }
+        public string ReportCreated { get; set; }
+
+
+        public bool Equals(ImpactAnalysisAllReportRow expected)
+        {
+            if (ReferenceEquals(expected, null)) throw new ArgumentNullException("expected");
+
+            var result = expected.Study.Contains(Study, StringComparison.OrdinalIgnoreCase)
+                         && DictionaryType.IsNullOrEqualsIfRequired(expected.DictionaryType)
+                         && ToVersion.IsNullOrEqualsIfRequired(expected.ToVersion)
+                         && ToSynonymList.IsNullOrEqualsIfRequired(expected.ToSynonymList);
+
+
+            return result;
+        }
+
+        public string ToString()
+        {
+            var response =
+                $"Study: {Study}, DictionaryType: {DictionaryType}, ToVersion: {ToVersion}, ToSynonymList: {ToSynonymList}";
+
+            return response;
+        }
+
+    }
+}
---
+++ Coder.DeclarativeBrowser/Models/GridModels/ImpactAnalysisReportDetailRow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coder.DeclarativeBrowser.ExtensionMethods;
+using Microsoft.VisualBasic.ApplicationServices;
+
+
+namespace Coder.DeclarativeBrowser.Models.GridModels
+{
+
+    namespace Coder.DeclarativeBrowser.Models.GridModels
+    {
+        public class ImpactAnalysisReportDetailRow
+        {
+            public string Verbatim         { get; set; }
+            public string CurrentTerm      { get; set; }
+            public string CurrentCode      { get; set; }
+            public string CurrentCodedPath { get; set; }
+            public string TermInNewVersion { get; set; }
+            public string CodeInNewVersion { get; set; }
+            public string NewCodedPath     { get; set; }
+
+
+            public bool Equals(ImpactAnalysisReportDetailRow expected)
+            {
+                if (ReferenceEquals(expected, null)) throw new ArgumentNullException("expected");
+
+                var result = expected.Verbatim.Contains(Verbatim, StringComparison.OrdinalIgnoreCase)
+                             && CurrentTerm.IsNullOrEqualsIfRequired(expected.CurrentTerm)
+                             && CurrentCode.IsNullOrEqualsIfRequired(expected.CurrentCode)
+                             && CurrentCodedPath.IsNullOrEqualsIfRequired(expected.CurrentCodedPath)
+                             && TermInNewVersion.IsNullOrEqualsIfRequired(expected.TermInNewVersion)
+                             && CodeInNewVersion.IsNullOrEqualsIfRequired(expected.CodeInNewVersion)
+                             && NewCodedPath.IsNullOrEqualsIfRequired(expected.NewCodedPath);
+
+
+                return result;
+            }
+
+            public string ToString()
+            {
+                var response =
+                    $"Verbatim: {Verbatim}, CurrentTerm: {CurrentTerm}, CurrentCode: {CurrentCode}, " +
+                    $"CurrentCodedPath: {CurrentCodedPath}, TermInNewVersion: {TermInNewVersion}, CodeInNewVersion: {CodeInNewVersion}, NewCodedPath: {NewCodedPath}";
+
+                return response;
+            }
+
+        }
+    }
+}
