25cd8395349eabf218d2321b1079e199cb92ab91
Sonali Malik <smalik@mdsol.com>
2016-06-01 20:42:18 -0400

    both scenarios run

--- Coder.EndToEndTests/Features/specENG_DynamicSearchList.feature
+++ Coder.EndToEndTests/Features/specENG_DynamicSearchList.feature
@@ -13,9 +13,9 @@ Scenario: Standard verbatim and supplemental fields using a control type of Dyna
 	And Rave Modules App Segment is loaded
  	And a Rave Coder setup with the following options
   	 | Form  | Field        | Dictionary   | Locale   | CodingLevel    | Priority | IsApprovalRequired | IsAutoApproval | SupplementalTerms  |
-  	 | ETE19 | Coding Field | <Dictionary> |          | PRODUCTSYNONYM | 1        | true               | true           | DSearchlist LL Sup |
+  	 | ETE19 | Coding Field | <Dictionary> |          | PRODUCTSYNONYM | 1        | true               | true           | DSearchlist Std Sup |
 	When a Rave Draft is published and pushed using draft "<DraftName>" for Project "<StudyName>" to environment "Prod"
-	And adding a new subject "TST"
+	When adding a new subject "TST"
 	And adding a new verbatim term to form "ETE19"
  	| Field                                    | Value                    | ControlType       |
  	| Coding Field                             | child advil cold extreme |                   |
@@ -23,13 +23,13 @@ Scenario: Standard verbatim and supplemental fields using a control type of Dyna
     And Coder App Segment is loaded
 	Then the "child advil cold extreme" task has the following supplemental information
 	| Term                   | Value   |
-	| ETE19.DSEARCHLISTLLSUP | DarkRed |
+	| ETE19.DSEARCHLISTSTDSUP | DarkRed |
 	And task "child advil cold extreme" should contain the following source term information
        | Source System | Study              | Dictionary            | Locale | Term                     | Level      | Priority |
        | Rave EDC      | <StudyDisplayName> | WhoDrugDDEB2 - 200703 | ENG    | child advil cold extreme | Trade Name | 1        |
 	When task "child advil cold extreme" is coded to term "CHILDRENS ADVIL COLD" at search level "Trade Name" with code "010502 01 015" at level "TN" and a synonym is created	
 	And Rave Modules App Segment is loaded
-	Then the coding decision for verbatim "child advil cold extreme" on form "ETE19" for field "Log Search List Coding Field" contains the following data
+	Then the coding decision for verbatim "child advil cold extreme" on form "ETE19" for field "Coding Field" contains the following data
       | Level          | Code          | Term Path                                         |
       | ATC            | M             | MUSCULO-SKELETAL SYSTEM                           |
       | ATC            | M01           | ANTIINFLAMMATORY AND ANTIRHEUMATIC PRODUCTS       |
@@ -39,32 +39,31 @@ Scenario: Standard verbatim and supplemental fields using a control type of Dyna
       | PRODUCTSYNONYM | 010502 01 015 | CHILDRENS ADVIL COLD                              |
 
 	  
-@DFT
+@VAL
 @PB92926DSL.005LLSUP
 @Release2016.1.0
 Scenario: Log line verbatim and supplemental fields using a control type of Dynamic Search List will be successfully coded.
-#This scenario should be updated based on the above scenarios for LL fields. See above comments.
-	Given a Rave project registration with dictionary "WhoDrugDDEB2 ENG 200703"
+	Given a Rave project registration with dictionary "WhoDrug-DDE-B2 ENG 200703"
 	And Rave Modules App Segment is loaded
  	And a Rave Coder setup with the following options
-  	 | Form  | Field        	| Dictionary   | Locale   | CodingLevel 			| Priority | IsApprovalRequired | IsAutoApproval | SupplementalTerms             |
-  	 | ETE19 | LL Coding Field | <Dictionary> | <Locale> | PRODUCTSYNONYM          | 1        | true               | true           |  DSearchlist LL Sup			|
-	When a Rave Draft is published and pushed using draft "<Draft>" for Project "<StudyName>" to environment "Prod"
+  	 | Form  | Field           | Dictionary   | Locale | CodingLevel    | Priority | IsApprovalRequired | IsAutoApproval | SupplementalTerms  |
+  	 | ETE19 | LL Coding Field | <Dictionary> |        | PRODUCTSYNONYM | 1        | true               | true           | DSearchlist LL Sup |
+	When a Rave Draft is published and pushed using draft "<DraftName>" for Project "<StudyName>" to environment "Prod"
 	And adding a new subject "TST"
 	And adding a new verbatim term to form "ETE19"
- 	| Field              | Value                    | ControlType       |
- 	| Log Coding Field   | child advil cold extreme | LongText          |
- 	| DSearchlist LL Sup | Sup1                     | DynamicSearchList |
+ 	| Field                                        | Value                    | ControlType       |
+ 	| Log Coding Field                             | child advil cold extreme | SearchList        |
+ 	| Log Dynamic Search List Supplemental Field B | Sup1                     | DynamicSearchList |
 	And Coder App Segment is loaded
-	Then I verify the following Supplemental information is displayed
-       | Supplemental Term      | Supplemental Value |
-       | ETE19.DSEARCHLISTLLSUP | Sup1               |
-	And task "child advil cold extreme" should contain the following source term information
+	Then the "child advil cold extreme" task has the following supplemental information
+	   | Term                   | Value |
+	   | ETE19.DSEARCHLISTLLSUP | Sup1  |
+    And task "child advil cold extreme" should contain the following source term information
        | Source System | Study              | Dictionary            | Locale | Term                     | Level      | Priority |
        | Rave EDC      | <StudyDisplayName> | WhoDrugDDEB2 - 200703 | ENG    | child advil cold extreme | Trade Name | 1        |
 	When task "child advil cold extreme" is coded to term "CHILDRENS ADVIL COLD" at search level "Trade Name" with code "010502 01 015" at level "TN" and a synonym is created	
 	And Rave Modules App Segment is loaded
-	Then the coding decision for verbatim "child advil cold extreme" on form "ETE19" for field "Log Search List Coding Field" contains the following data
+	Then the coding decision for verbatim "child advil cold extreme" on form "ETE19" for field "Log Coding Field" contains the following data
       | Level          | Code          | Term Path                                         |
       | ATC            | M             | MUSCULO-SKELETAL SYSTEM                           |
       | ATC            | M01           | ANTIINFLAMMATORY AND ANTIRHEUMATIC PRODUCTS       |
--- Coder.EndToEndTests/Features/specENG_DynamicSearchList.feature.cs
+++ Coder.EndToEndTests/Features/specENG_DynamicSearchList.feature.cs
@@ -111,14 +111,14 @@ this.ScenarioSetup(scenarioInfo);
                         "1",
                         "true",
                         "true",
-                        "DSearchlist LL Sup"});
+                        "DSearchlist Std Sup"});
 #line 14
   testRunner.And("a Rave Coder setup with the following options", ((string)(null)), table1, "And ");
 #line 17
  testRunner.When("a Rave Draft is published and pushed using draft \"<DraftName>\" for Project \"<Stud" +
                     "yName>\" to environment \"Prod\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line 18
- testRunner.And("adding a new subject \"TST\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.When("adding a new subject \"TST\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
             TechTalk.SpecFlow.Table table2 = new TechTalk.SpecFlow.Table(new string[] {
                         "Field",
@@ -141,7 +141,7 @@ this.ScenarioSetup(scenarioInfo);
                         "Term",
                         "Value"});
             table3.AddRow(new string[] {
-                        "ETE19.DSEARCHLISTLLSUP",
+                        "ETE19.DSEARCHLISTSTDSUP",
                         "DarkRed"});
 #line 24
  testRunner.Then("the \"child advil cold extreme\" task has the following supplemental information", ((string)(null)), table3, "Then ");
@@ -202,7 +202,7 @@ this.ScenarioSetup(scenarioInfo);
                         "CHILDRENS ADVIL COLD"});
 #line 32
  testRunner.Then("the coding decision for verbatim \"child advil cold extreme\" on form \"ETE19\" for f" +
-                    "ield \"Log Search List Coding Field\" contains the following data", ((string)(null)), table5, "Then ");
+                    "ield \"Coding Field\" contains the following data", ((string)(null)), table5, "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
@@ -210,21 +210,21 @@ this.ScenarioSetup(scenarioInfo);
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("Log line verbatim and supplemental fields using a control type of Dynamic Search " +
             "List will be successfully coded.")]
-        [NUnit.Framework.CategoryAttribute("DFT")]
+        [NUnit.Framework.CategoryAttribute("VAL")]
         [NUnit.Framework.CategoryAttribute("PB92926DSL.005LLSUP")]
         [NUnit.Framework.CategoryAttribute("Release2016.1.0")]
         public virtual void LogLineVerbatimAndSupplementalFieldsUsingAControlTypeOfDynamicSearchListWillBeSuccessfullyCoded_()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Log line verbatim and supplemental fields using a control type of Dynamic Search " +
                     "List will be successfully coded.", new string[] {
-                        "DFT",
+                        "VAL",
                         "PB92926DSL.005LLSUP",
                         "Release2016.1.0"});
 #line 45
 this.ScenarioSetup(scenarioInfo);
+#line 46
+ testRunner.Given("a Rave project registration with dictionary \"WhoDrug-DDE-B2 ENG 200703\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line 47
- testRunner.Given("a Rave project registration with dictionary \"WhoDrugDDEB2 ENG 200703\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line 48
  testRunner.And("Rave Modules App Segment is loaded", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             TechTalk.SpecFlow.Table table6 = new TechTalk.SpecFlow.Table(new string[] {
@@ -241,18 +241,18 @@ this.ScenarioSetup(scenarioInfo);
                         "ETE19",
                         "LL Coding Field",
                         "<Dictionary>",
-                        "<Locale>",
+                        "",
                         "PRODUCTSYNONYM",
                         "1",
                         "true",
                         "true",
                         "DSearchlist LL Sup"});
-#line 49
+#line 48
   testRunner.And("a Rave Coder setup with the following options", ((string)(null)), table6, "And ");
+#line 51
+ testRunner.When("a Rave Draft is published and pushed using draft \"<DraftName>\" for Project \"<Stud" +
+                    "yName>\" to environment \"Prod\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line 52
- testRunner.When("a Rave Draft is published and pushed using draft \"<Draft>\" for Project \"<StudyNam" +
-                    "e>\" to environment \"Prod\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 53
  testRunner.And("adding a new subject \"TST\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             TechTalk.SpecFlow.Table table7 = new TechTalk.SpecFlow.Table(new string[] {
@@ -262,24 +262,24 @@ this.ScenarioSetup(scenarioInfo);
             table7.AddRow(new string[] {
                         "Log Coding Field",
                         "child advil cold extreme",
-                        "LongText"});
+                        "SearchList"});
             table7.AddRow(new string[] {
-                        "DSearchlist LL Sup",
+                        "Log Dynamic Search List Supplemental Field B",
                         "Sup1",
                         "DynamicSearchList"});
-#line 54
+#line 53
  testRunner.And("adding a new verbatim term to form \"ETE19\"", ((string)(null)), table7, "And ");
-#line 58
+#line 57
  testRunner.And("Coder App Segment is loaded", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             TechTalk.SpecFlow.Table table8 = new TechTalk.SpecFlow.Table(new string[] {
-                        "Supplemental Term",
-                        "Supplemental Value"});
+                        "Term",
+                        "Value"});
             table8.AddRow(new string[] {
                         "ETE19.DSEARCHLISTLLSUP",
                         "Sup1"});
-#line 59
- testRunner.Then("I verify the following Supplemental information is displayed", ((string)(null)), table8, "Then ");
+#line 58
+ testRunner.Then("the \"child advil cold extreme\" task has the following supplemental information", ((string)(null)), table8, "Then ");
 #line hidden
             TechTalk.SpecFlow.Table table9 = new TechTalk.SpecFlow.Table(new string[] {
                         "Source System",
@@ -297,14 +297,14 @@ this.ScenarioSetup(scenarioInfo);
                         "child advil cold extreme",
                         "Trade Name",
                         "1"});
-#line 62
- testRunner.And("task \"child advil cold extreme\" should contain the following source term informat" +
+#line 61
+    testRunner.And("task \"child advil cold extreme\" should contain the following source term informat" +
                     "ion", ((string)(null)), table9, "And ");
-#line 65
+#line 64
  testRunner.When("task \"child advil cold extreme\" is coded to term \"CHILDRENS ADVIL COLD\" at search" +
                     " level \"Trade Name\" with code \"010502 01 015\" at level \"TN\" and a synonym is cre" +
                     "ated", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line 66
+#line 65
  testRunner.And("Rave Modules App Segment is loaded", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             TechTalk.SpecFlow.Table table10 = new TechTalk.SpecFlow.Table(new string[] {
@@ -335,9 +335,9 @@ this.ScenarioSetup(scenarioInfo);
                         "PRODUCTSYNONYM",
                         "010502 01 015",
                         "CHILDRENS ADVIL COLD"});
-#line 67
+#line 66
  testRunner.Then("the coding decision for verbatim \"child advil cold extreme\" on form \"ETE19\" for f" +
-                    "ield \"Log Search List Coding Field\" contains the following data", ((string)(null)), table10, "Then ");
+                    "ield \"Log Coding Field\" contains the following data", ((string)(null)), table10, "Then ");
 #line hidden
             this.ScenarioCleanup();
         }
