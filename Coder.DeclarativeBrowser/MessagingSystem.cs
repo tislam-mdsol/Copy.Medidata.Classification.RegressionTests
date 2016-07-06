8486482a8f43bcde29ec45471f38b1212cea4395
David Jetter <djetter@mdsol.com>
2016-07-06 15:42:55 -0400

    added hosting.aws reference to regression tests and removed implementation of IMessageHostInitializer from Declarative Browser

--- Coder.DeclarativeBrowser/MessagingSystem.cs
+++ Coder.DeclarativeBrowser/MessagingSystem.cs
@@ -6,36 +6,13 @@ using Medidata.MessagingBridge;
 
 namespace Coder.DeclarativeBrowser
 {
-    public class MessagingSystem : IMessageHostingInitializer
+    public class MessagingSystem
     {
-        private static Boolean _IsInitialized;
-        private static Object _Sync = new Object();
-
         public static void Start()
         {
-            var messagingSystem = new MessagingSystem();
-            messagingSystem.Initialize();
-        }
-
-        public void Initialize()
-        {
-            if (_IsInitialized)
-            {
-                return;
-            }
-
-            lock (_Sync)
-            {
-                if (_IsInitialized)
-                {
-                    return;
-                }
-
-                _IsInitialized = true;
-                FactoryInitializationHelper.SetEntryAssemblyFromType<CoderDeclarativeBrowser>();
-                var messageHostInitializer = Factory.Build<IMessageHostingInitializer>();
-                messageHostInitializer.Initialize();
-            }
+            FactoryInitializationHelper.SetEntryAssemblyFromType<CoderDeclarativeBrowser>();
+            var messageHostInitializer = Factory.Build<IMessageHostingInitializer>();
+            messageHostInitializer.Initialize();
         }
     }
 }
